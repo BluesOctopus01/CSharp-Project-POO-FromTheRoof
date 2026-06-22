0764a6fb697ee87d51d726bca7868001f220b8b4
BluesOctopus01 <metens.thomas@outlook.be>
2026-06-22 13:28:08 +0200

    feat: add methods to StatShett.cs

--- FromTheRoof/Class/StatSheet.cs
+++ FromTheRoof/Class/StatSheet.cs
@@ -5,24 +5,33 @@ namespace FromTheRoof.Class;
 public class StatSheet
 {
     private int _energy = 100;
-    private int _motivation = 50;
+    private int _motivation = 100;
     private int _stress = 0;
     private double _money = 25.0;
 
-    public void ModifyEnergy()
+    public bool IsExhausted => _energy <= 0;
+    public bool IsBurnedOut => _stress >= 100;
+    public bool IsBroke => _money <= 0;
+
+    public bool IsMotivated => _motivation >= 50;
+    public void ModifyEnergy(int value)
     {
-        throw new NotImplementedException("ModifyEnergy n'est pas encore implémenter");
+        _energy += value;
+        _energy = Math.Clamp(_energy, 0, 100);
     }
-    public void ModifyMotivation()
+    public void ModifyMotivation(int value)
     {
-        throw new NotImplementedException("ModifyMotivation n'est pas encore implémenter");
+        _motivation += value;
+        _motivation = Math.Clamp(_motivation, 0, 100);
     }
-    public bool ModifyStress()
+    public void ModifyStress(int value)
     {
-        throw new NotImplementedException("ModifyStress n'est pas encore implémenter");
+        _stress += value;
+        _stress = Math.Clamp(_stress, 0, 100);
     }
-    public bool ModifyMoney()
+    public void ModifyMoney(double value)
     {
-        throw new NotImplementedException("ModifyMoney n'est pas encore implémenter");
+        _money += value;
+        _money = Math.Clamp(_money, 0, 9999);
     }
 }
